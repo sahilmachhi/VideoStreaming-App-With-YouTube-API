d963b191506e385e288128826defce3c0da7478c
whonix2904 <sahilmachhi29@gmail.com>
2024-09-18 21:40:35 +0530

    changed search suggestion looks

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -3,13 +3,16 @@ import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { searchFromApi } from "../../Utils/FetchAPI";
+import NorthWestIcon from "@mui/icons-material/NorthWest";
 
 function SearchBar() {
-  const [suggetion, setSuggetion] = useState("none");
   const navigate = useNavigate();
+  // const [activeSuggestion, setActiveSuggestion] = useState(0);
+  const [suggetion, setSuggetion] = useState("none");
   const [searchKey, setSearchKey] = useState([]);
   const [search, setSearch] = useState("");
   const debounceTimer = useRef(null);
+  const suggestionListRef = useRef(null);
   useEffect(() => {
     // Clear the previous timer before setting a new one
     if (debounceTimer.current) {
@@ -30,11 +33,31 @@ function SearchBar() {
     return () => clearTimeout(debounceTimer.current);
   }, [search]);
 
-  console.log(searchKey);
+  // const handleKeyDown = (e) => {
+  //   if (e.key === "ArrowDown") {
+  //     setActiveSuggestion((prevIndex) =>
+  //       prevIndex < searchKey.length - 1 ? prevIndex + 1 : 0
+  //     );
+  //   } else if (e.key === "ArrowUp") {
+  //     setActiveSuggestion((prevIndex) =>
+  //       prevIndex > 0 ? prevIndex - 1 : searchKey.length - 1
+  //     );
+  //   }
+  //   // } else if (e.key === "Enter" && searchKey.length > 0) {
+  //   //   e.preventDefualt();
+  //   //   setSearch(searchKey[activeSuggestion].title);
+  //   //   navigate(`/search/${search}`);
+  //   //   setSuggetion("none");
+  //   // }
+  // };
+
   const searchSubmit = (e) => {
     e.preventDefault();
-    setSuggetion("none");
+    // setSearch(searchKey[activeSuggestion].title);
     navigate(`/search/${search}`);
+    setSuggetion("none");
+    // console.log(`search key:${searchKey[activeSuggestion].title}`);
+    // console.log(`search var:${search}`);
   };
   return (
     <>
@@ -57,27 +80,52 @@ function SearchBar() {
           placeholder="search here"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
+          // onKeyDown={handleKeyDown}
         />
-        <IconButton aria-label="submit-search" id="search-submit">
+        <IconButton
+          aria-label="submit-search"
+          id="search-submit"
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(`/search/${search}`);
+            setSuggetion("none");
+          }}
+        >
           <SearchIcon className="text-white" />
         </IconButton>
         <div
-          className="bg-white absolute left-0 px-6 pl-3 top-12 z-20 w-full  flex-col gap-2"
+          className="absolute left-0 top-12 z-20 w-full bg-gray-700 flex-col rounded-md"
           style={{ display: suggetion }}
+          ref={suggestionListRef}
         >
           {searchKey.slice(0, 5).map((searchKey, index) => {
-            console.log(searchKey.title);
+            // const isActive = index === activeSuggestion;
             return (
               <div
                 key={index}
-                onClick={(e) => {
-                  e.preventDefault();
-                  navigate(`/search/${searchKey.title}`);
-                  setSearch("");
-                  setSuggetion("none");
-                }}
+                className={`py-2 pl-5 flex flex-row justify-between cursor-pointer bg-gray-700 hover:bg-gray-950 w-full`}
               >
-                {searchKey.title}
+                <p
+                  className="text-white h-full w-full"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate(`/search/${searchKey.title}`);
+                    setSearch(searchKey.title);
+                    setSuggetion("none");
+                  }}
+                >
+                  {searchKey.title.length > 45
+                    ? searchKey.title.substring(0, 45) + "..."
+                    : searchKey.title}
+                </p>
+                <NorthWestIcon
+                  className="w-full text-white h-full"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setSearch(searchKey.title);
+                    setSuggetion("none");
+                  }}
+                />
               </div>
             );
           })}
